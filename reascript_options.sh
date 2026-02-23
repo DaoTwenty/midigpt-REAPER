32f49cb6cfd0f812d5f0939bd882537b7c0326bd
Paul Triana <paul_triana@sfu.ca>
2026-02-23 13:57:58 -0800

    Fix server start missing cli flag

--- reascript_options.sh
+++ reascript_options.sh
@@ -2,6 +2,6 @@ python3 - <<'EOF'
 import sysconfig, pathlib
 libdir = pathlib.Path(sysconfig.get_config_var("LIBDIR"))
 print(f"Python DLL path: {libdir}")
-for p in libdir.glob("*.dylib"):
+for p in libdir.glob("libpython*.dylib"):
     print(f"Python .dylib filename: {p.name}")
 EOF
--- start_mmm_server.sh
+++ start_mmm_server.sh
@@ -1,3 +1,3 @@
 source .venv/bin/activate
 cd src/Scripts/MMM/
-python3 MMM_server.py models/config.json
+python3 MMM_server.py --config models/config.json
